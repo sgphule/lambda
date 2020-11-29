81132406dfb6dfbae067e967a3e721449d52da40
sgphule <sgphule.gmail.com>
2020-11-29 19:01:04 +0100

    Using Predicate functional Interface

--- src/io/sudarshan/JavaEightWay.java
+++ src/io/sudarshan/JavaEightWay.java
@@ -4,17 +4,18 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class JavaEightWay {
 	//Java 8 way
-		public static void main(String[] arge) {
-			List<Person> people = Arrays.asList(
-					new Person("Sudarshan", "Phule", 99),
-					new Person("Satyajit", "Phule", 89),
-					new Person("abcd","Xyz",79),
-					new Person("lmn", "Pqr", 9),
-					new Person("www", "Ssl", 19)
-					);
+	public static void main(String[] arge) {
+		List<Person> people = Arrays.asList(
+			new Person("Sudarshan", "Phule", 99),
+			new Person("Satyajit", "Phule", 89),
+			new Person("abcd","Xyz",79),
+			new Person("lmn", "Pqr", 9),
+			new Person("www", "Ssl", 19)
+			);
 			//Sort list by last name
 			Collections.sort(people, (p1, p2) -> p1.getLastName().compareTo(p2.getLastName()));			
 			//Create a method to print all elements in the list
@@ -28,12 +29,12 @@ public class JavaEightWay {
 			
 			System.out.println("\nPrinting list for First name starting with S");
 			printConditionally(people,p -> p.getFirstName().startsWith("S"));
-		}
-		private static void printConditionally(List<Person> people, Condition condition) {
-			for(Person p : people) {
-				if(condition.test(p)) {
-					System.out.println(p);
-				}
+	}
+	private static void printConditionally(List<Person> people, Predicate<Person> predicate) {
+		for(Person p : people) {
+			if(predicate.test(p)) {
+				System.out.println(p);
 			}
 		}
+	}
 }	
