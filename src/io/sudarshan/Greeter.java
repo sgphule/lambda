5e17c843994a6536411efae0c3ae9e42ce1b0d95
sgphule <sgphule.gmail.com>
2020-11-29 17:49:16 +0100

    Type Inference Lambda

--- src/io/sudarshan/Greeter.java
+++ src/io/sudarshan/Greeter.java
@@ -7,16 +7,15 @@ public class Greeter {
 	public static void main(String[] args) {
 		Greeter greeter = new Greeter();
 		HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
-		Greeting lambdaGreeting = () -> System.out.println("Hello world");
 		Greeting innerClassGreeting = new Greeting() {
 			public void perform() {
 				System.out.println("Hello World");
 			}
 		};
 		
-		greeter.greet(helloWorldGreeting);//helloWorldGreeting.perform();
-		greeter.greet(lambdaGreeting);//lambdaGreeting.perform();
-		greeter.greet(innerClassGreeting);//innerClassGreeting.perform();
+		greeter.greet(helloWorldGreeting);						//helloWorldGreeting.perform();
+		greeter.greet(() -> System.out.println("Hello world"));	//lambdaGreeting.perform();
+		greeter.greet(innerClassGreeting);						//innerClassGreeting.perform();
 		
 	}
 }
---
+++ src/io/sudarshan/TypeInferenceExample.java
@@ -0,0 +1,14 @@
+package io.sudarshan;
+
+public class TypeInferenceExample {
+
+	public static void main(String[] args) {
+		printLambda(s -> s.length());
+	}
+	public static void printLambda(StringLengthLambda l) {
+		System.out.println(l.getLength("Hello Lambda"));
+	}
+	interface StringLengthLambda{
+		int getLength(String s);
+	}
+}
