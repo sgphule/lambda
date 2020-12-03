8aef0d58fdd5f49635982894772bf234e5b6bb47
sgphule <sgphule.gmail.com>
2020-12-03 20:14:29 +0100

    One way of handling exception

--- src/io/sudarshan/exception/ExceptionHandlingExample.java
+++ src/io/sudarshan/exception/ExceptionHandlingExample.java
@@ -6,8 +6,15 @@ public class ExceptionHandlingExample {
 
 	public static void main(String[] args) {
 		int [] someNumbers = {1, 2, 3, 4};
-		int key = 2;
-		process(someNumbers, key, (v,k) -> System.out.println(v/k));
+		int key = 0;
+		process(someNumbers, key, (v,k) -> {
+			try{
+				System.out.println(v/k);
+			}
+			catch (ArithmeticException e) {
+				System.out.println("An Arithmatic exception happened");
+			}
+		});
 	}
 
 	private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer) {
