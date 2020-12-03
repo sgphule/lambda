e8ea6edbe92dc69883a6f5916b59d22eeb6a5e36
sgphule <sgphule.gmail.com>
2020-12-03 20:21:02 +0100

    No op

--- src/io/sudarshan/exception/ExceptionHandlingExample.java
+++ src/io/sudarshan/exception/ExceptionHandlingExample.java
@@ -6,22 +6,17 @@ public class ExceptionHandlingExample {
 
 	public static void main(String[] args) {
 		int [] someNumbers = {1, 2, 3, 4};
-		int key = 0;
-		process(someNumbers, key, (v,k) -> {
-			try{
-				System.out.println(v/k);
-			}
-			catch (ArithmeticException e) {
-				System.out.println("An Arithmatic exception happened");
-			}
-		});
+		int key = 2;
+		process(someNumbers, key, wrapperLambda((v,k) -> System.out.println(v/k)));
 	}
 
 	private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer) {
 		for(int i:someNumbers) {
 			consumer.accept(i, key);
 		}
-		
+	}
+	private static BiConsumer<Integer, Integer> wrapperLambda(BiConsumer<Integer, Integer> consumer) {
+		return consumer;
 	}
 
 }
