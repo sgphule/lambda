8ce9af31ec00ea884ed5914f3785a894e702dd23
sgphule <sgphule.gmail.com>
2020-12-12 18:53:11 +0100

    execute method without seperate thread and lambda

---
+++ src/io/collections/MethodReferenceCollecetionsExample.java
@@ -0,0 +1,11 @@
+package io.collections;
+
+public class MethodReferenceCollecetionsExample {
+
+	public static void main(String[] args) {
+		printMessage();
+	}
+	public static void printMessage() {
+		System.out.println("Hello");
+	}
+}
