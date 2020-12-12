280d0ad41469cb2840a4997d08b724e9daa35e68
sgphule <sgphule.gmail.com>
2020-12-12 18:00:56 +0100

    With lamda expression

--- src/io/sudarshan/exception/ClosuresExample.java
+++ src/io/sudarshan/exception/ClosuresExample.java
@@ -5,13 +5,7 @@ public class ClosuresExample {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 20;
-		doProcess(a, new Process() {
-			public void process(int i) {
-				System.out.println(i + b);
-				
-			}
-		});
-
+		doProcess(a, i -> System.out.println(i + b));
 	}
 	public static void doProcess(int i, Process p) {
 		p.process(i);
