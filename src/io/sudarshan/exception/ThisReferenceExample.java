f340454e80ca87234ba92df283fad719a49a3b2c
sgphule <sgphule.gmail.com>
2020-12-12 18:42:35 +0100

    Lambda with this reference

--- src/io/sudarshan/exception/ThisReferenceExample.java
+++ src/io/sudarshan/exception/ThisReferenceExample.java
@@ -5,12 +5,24 @@ public class ThisReferenceExample {
 	public void doProcess(int i, Process p) {
 		p.process(i);
 	}
+	public void execute() {
+		//this == ?
+		doProcess(20, i -> {
+			System.out.println("Value of i is:" + i);
+			System.out.println(this);   
+		});
+	}
 	public static void main(String[] args) {
 		ThisReferenceExample thisReferenceExample = new ThisReferenceExample();
+		/*
 		thisReferenceExample.doProcess(20, i -> {
 				System.out.println("Value of i is:" + i);
 				//System.out.println(this);   This will not work
 			});
+		*/
+		thisReferenceExample.execute();
 	}
-
+	public String toString() {
+		return "This is the main ThisReferenceExample class instance";
+	}
 }
