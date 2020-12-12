311be6a1670dec04655dd32d114224f88529d9d2
sgphule <sgphule.gmail.com>
2020-12-12 18:34:25 +0100

    Lambda this reference is same as outside lambda

--- src/io/sudarshan/exception/ThisReferenceExample.java
+++ src/io/sudarshan/exception/ThisReferenceExample.java
@@ -7,19 +7,10 @@ public class ThisReferenceExample {
 	}
 	public static void main(String[] args) {
 		ThisReferenceExample thisReferenceExample = new ThisReferenceExample();
-		thisReferenceExample.doProcess(20, new Process() {
-
-			@Override
-			public void process(int i) {
+		thisReferenceExample.doProcess(20, i -> {
 				System.out.println("Value of i is:" + i);
-				System.out.println(this);   
-			}
-			
-			public String toString() {
-				return "This is the anonymous inner class";
-			}
-		});
-
+				//System.out.println(this);   This will not work
+			});
 	}
 
 }
